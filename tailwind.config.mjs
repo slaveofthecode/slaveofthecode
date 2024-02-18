dfda6cc48fbf574365fa15d17aee57ce1c489438
Gustavo Lopez <gustavo@getkalto.com>
2024-02-18 18:11:58 -0300

    WIP toogle skin mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "class",
+	// darkMode: "media",
+	darkMode: ["class", '[data-skin-mode="dark"]'],
 	theme: {
 		extend: {
 			animation: {

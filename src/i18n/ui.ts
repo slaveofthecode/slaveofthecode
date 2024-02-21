199a3827ad523252e0e7b6a4221f570b1c786289
Gustavo Lopez <gustavo@getkalto.com>
2024-02-21 17:35:35 -0300

    Translate language to Spanish

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,14 +1,42 @@
+import { getJsonCV } from "@/utils";
+
+const resumeEN = getJsonCV("en");
+const resumeSP = getJsonCV("sp");
+const resumeDE = getJsonCV("de");
+
 export const ui = {
 	en: {
 		"section.aboutMe": "About me",
 		"section.experience": "Experience",
+		"section.experience.justLastFiveJobs": "just last five jobs",
+		"section.education": "Education",
+		"section.skills": "Skills",
+		"section.projects": "Projects",
+		"section.projects.onlyMostImportant": "...only the most important",
+		"section.certifications": "Certifications",
+		resume: resumeEN,
 	},
 	sp: {
 		"section.aboutMe": "Sobre mi",
 		"section.experience": "Experiencia",
+		"section.experience.justLastFiveJobs":
+			"solo los últimos cinco trabajos",
+		"section.education": "Educación",
+		"section.skills": "Habilidades",
+		"section.projects": "Proyectos",
+		"section.projects.onlyMostImportant": "...solo los más importantes",
+		"section.certifications": "Certificaciones",
+		resume: resumeSP,
 	},
 	de: {
 		"section.aboutMe": "Über mich",
 		"section.experience": "Erfahrung",
+		"section.experience.justLastFiveJobs": "nur die letzten fünf Jobs",
+		"section.education": "Bildung",
+		"section.skills": "Fähigkeiten",
+		"section.projects": "Projekte",
+		"section.projects.onlyMostImportant": "...nur die wichtigsten",
+		"section.certifications": "Zertifizierungen",
+		resume: resumeDE,
 	},
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
-import CV from "@cv" assert { type: "json" };
+import CV_DE from "@cv/resume-de.json" assert { type: "json" };
+import CV_SP from "@cv/resume-sp.json" assert { type: "json" };
+import CV_EN from "@cv/resume.json" assert { type: "json" };
 
-export const getJsonCV = () => CV;
+export const getJsonCV = (lang: string) => {
+	return lang === "sp" ? CV_SP : lang === "de" ? CV_DE : CV_EN;
+};

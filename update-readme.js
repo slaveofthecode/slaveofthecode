ed232813a258822b38fafd3fffa1b66c880b8721
Gustavo Lopez <gustavo.lopez@nybblegroup.com>
2025-04-07 22:27:03 -0300

    Dynamic change gif everyday

---
+++ update-readme.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const gifs = {
+	0: 'https://media.giphy.com/media/YQitE4YNQNahy/giphy.gif', // Sunday
+	1: 'https://media.giphy.com/media/L1R1tvI9svkIWwpVYr/giphy.gif', // Monday
+	2: 'https://media.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.gif', // Tuesday
+	3: 'https://media.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif', // Wednesday
+	4: 'https://media.giphy.com/media/13HgwGsXF0aiGY/giphy.gif', // Thursday
+	5: 'https://media.giphy.com/media/26xBwdIuRJiAIqHwA/giphy.gif', // Friday
+	6: 'https://media.giphy.com/media/LMcB8XospGZO8UQq87/giphy.gif', // Saturday
+};
+
+const today = new Date().getDay();
+const selectedGif = gifs[today];
+
+const readmePath = 'README.md';
+let readme = fs.readFileSync(readmePath, 'utf8');
+
+// Replace the GIF block
+const newBlock = `<div align="center">\n  <img src="${selectedGif}" width="450"/>\n</div>`;
+readme = readme.replace(
+	/<div align="center">[\s\S]*?<img src=".*?" width="450"\/>[\s\S]*?<\/div>/,
+	newBlock
+);
+
+fs.writeFileSync(readmePath, readme, 'utf8');
+console.log(`✅ README updated with today's GIF.`);
